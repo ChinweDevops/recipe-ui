f393b5cd2065c7d6b5eab929306e8f1f684d6f30
Emmanuel_Sewah <sewah2012@gmail.com>
2022-11-12 19:29:39 +0100

    changed footer

--- src/component/FooterSection.jsx
+++ src/component/FooterSection.jsx
@@ -1,12 +1,14 @@
-import './footerSection.css'
-import React from 'react'
+import "./footerSection.css";
+import React from "react";
 
 const FooterSection = () => {
   return (
-    <div className='footer'>
-      <h3>Designed and Developed by: <span>Chinwe Emele</span></h3>
+    <div className="footer">
+      <h3>
+        Designed and Developed with ❤️ by: <span>Emmanuel Sahr Sewah</span>
+      </h3>
     </div>
-  )
-}
+  );
+};
 
-export default FooterSection
+export default FooterSection;
